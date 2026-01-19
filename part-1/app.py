56bc11e531b879c6ceac7585df665aa78c4e9706
NEHAGAIDHANI <nehagaidhani9767@gmail.com>
2026-01-19 15:49:13 +0530

    Organizing project into part-1 folder

--- part-1/app.py
+++ part-1/app.py
@@ -63,10 +63,17 @@ def index():
 def add_sample_student():
     """Add a sample student to database (for testing)"""
     conn = get_db_connection()
-    conn.execute(
-        'INSERT INTO students (name, email, course) VALUES (?, ?, ?)',
-        ('John Doe', 'john@example.com', 'Python')  # ? are placeholders (safe from SQL injection)
+    students = [
+       ('Teena', 'teena@gmail.com', 'HTML'),
+       ('Meena', 'meena@gmail.com', 'CSS'),
+       ('Reena', 'reena@gmail.com', 'JAVA'),
+       ('Heena', 'heena@gmail.com', 'Python'),
+    ]
+    conn.executemany ( 
+       'INSERT INTO students (name,email,course)VALUES(?,?,?)',
+       students
     )
+    
     conn.commit()  # Don't forget to commit!
     conn.close()
     return 'Student added! <a href="/">Go back to home</a>'
